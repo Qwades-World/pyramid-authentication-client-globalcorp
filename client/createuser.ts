a552e94c661f9545bff4a48e5e83742e4d4ae1d1
Q Wade Billings <qwbillings@gmail.com>
2024-06-18 15:41:09 -0400

    Making progress

--- client/createuser.ts
+++ client/createuser.ts
@@ -1,14 +1,46 @@
-import { UserCredentials } from '../model/userCredentials';
+import { XMLHttpRequest } from "xhr2";
 
-export interface IUserService {
-    getUserCredentials(): UserCredentials;
+// URL of the Pyramid installation and the path to the API 3.0 REST methods
+var pyramidURL = "http://aebe29ce17b774358858f877b6cc71d3-1279237833.us-east-1.elb.amazonaws.com:8181/API3/";
+
+(async () => {
+    let token = await callApi("authentication/authenticateUser", {
+        "userName": "qwade",
+        "password": "Password"
+    }, "", false);
+
+    let currentUser = await callApi("access/getMe", {}, token);
+
+    let userId = currentUser.data[0];
+})();
+
+// ##### generic REST API calling method ##############
+async function callApi(path: string, data: object, token: string = "", parseResult: boolean = true): Promise<any> {
+    return new Promise((resolve, reject) => {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", pyramidURL + path, true);
+        xhttp.setRequestHeader("paToken", token);
+        xhttp.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                if (parseResult) {
+                    resolve(JSON.parse(xhttp.responseText));
+                } else {
+                    resolve(xhttp.responseText);
+                }
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhttp.statusText
+                });
+            }
+        };
+        xhttp.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhttp.statusText
+            });
+        };
+        xhttp.send(JSON.stringify(data));
+    });
 }
 
-const userService: IUserService = {
-    getUserCredentials(): UserCredentials {
-        return {
-            username: 'XXXX',
-            password: 'XXXX'
-        };
-    }
-};
