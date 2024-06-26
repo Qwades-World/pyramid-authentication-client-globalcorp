43e32581c094f767c79cec06725fe2d9fc7e951b
Q Wade Billings <qwbillings@gmail.com>
2024-06-26 11:19:22 -0400

    Got it working :)

--- client/createuser.ts
+++ client/createuser.ts
@@ -1,46 +1,31 @@
-import { XMLHttpRequest } from "xhr2";
+import axios from 'axios';
+import dotenv from "dotenv";
 
-// URL of the Pyramid installation and the path to the API 3.0 REST methods
-var pyramidURL = "http://aebe29ce17b774358858f877b6cc71d3-1279237833.us-east-1.elb.amazonaws.com:8181/API3/";
+dotenv.config();
 
-(async () => {
-    let token = await callApi("authentication/authenticateUser", {
-        "userName": "qwade",
-        "password": "Password"
-    }, "", false);
+const apiUrl = process.env.APIURL || "https://www.example.com";
+const apiKey = process.env.APIKEY || "THISISAKEY";
 
-    let currentUser = await callApi("access/getMe", {}, token);
+const data = {
+  lastName: "Smithy",
+  userName: "bob",
+  firstName: "Robert",
+  password: "password",
+  tenantId: process.env.TENANTID!, 
+  adminType: 0,
+  clientLicenseType: 100,
+  email: "bob@bob.com",
+  inheritanceType: "CreateUserDbObject"
+};
 
-    let userId = currentUser.data[0];
-})();
-
-// ##### generic REST API calling method ##############
-async function callApi(path: string, data: object, token: string = "", parseResult: boolean = true): Promise<any> {
-    return new Promise((resolve, reject) => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("POST", pyramidURL + path, true);
-        xhttp.setRequestHeader("paToken", token);
-        xhttp.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                if (parseResult) {
-                    resolve(JSON.parse(xhttp.responseText));
-                } else {
-                    resolve(xhttp.responseText);
-                }
-            } else {
-                reject({
-                    status: this.status,
-                    statusText: xhttp.statusText
-                });
-            }
-        };
-        xhttp.onerror = function () {
-            reject({
-                status: this.status,
-                statusText: xhttp.statusText
-            });
-        };
-        xhttp.send(JSON.stringify(data));
-    });
-}
-
+axios.post(apiUrl, data, {
+  headers: {
+    paToken: apiKey
+  }
+})
+.then(response => {
+  console.log('Success:', response.data);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
